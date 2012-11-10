07277da12cea702f45146a52ad4eba6c068c74d2
David McGrath <github.com@davidmcgrath.com>
2012-11-10 18:26:54 -0500

    refactored Emulator

--- ArcadiaEngine/_items/Emulator.cs
+++ ArcadiaEngine/_items/Emulator.cs
@@ -1,31 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+using System.IO;
+using System.Diagnostics;
 using System.Xml;
+
 
 namespace SomewhatGeeky.Arcadia.Engine
 {
     public class Emulator : GenericLibraryItem
     {
-        private string path;
-        private string argumentPattern;
-        private List<Platform> compatablePlatforms;
+        public const string ArgumentPatternFileVariable = "$(FilePath)";
+        public const string DefaultArgumentPattern = "\"" + ArgumentPatternFileVariable + "\"";
+
+        public string Path { get; set; }
+        public string ArgumentPattern { get; set; }
+        public ICollection<Platform> CompatablePlatforms { get; private set; }
 
         #region constructors
 
-        public Emulator(string name, ArcadiaLibrary parentGameLibrary)
+        public Emulator(string name = null, ArcadiaLibrary parentGameLibrary = null)
             : base(name, parentGameLibrary)
         {
-            compatablePlatforms = new List<Platform>();
+            CompatablePlatforms = new List<Platform>();
         }
+
         public Emulator(string name)
             : this(name, null)
         { }
+
         public Emulator(ArcadiaLibrary parentGameLibrary)
             : this(null, parentGameLibrary)
         { }
+
         public Emulator()
             : this(null, null)
         { }
@@ -33,42 +39,56 @@ namespace SomewhatGeeky.Arcadia.Engine
         #endregion
 
         #region xml
-        protected override void writeToXmlExtension(System.Xml.XmlWriter writer)
+
+        protected override void writeToXmlExtension(XmlWriter writer)
         {
             //write path
             if (PathIsSet)
+            {
                 writer.WriteAttributeString("path", Path);
+            }
 
             //write argument pattern
             if (ArgumentPatternIsSet)
+            {
                 writer.WriteAttributeString("argumentPattern", ArgumentPattern);
+            }
 
             //write platforms
             if (CompatablePlatforms.Count > 0)
             {
                 writer.WriteStartElement("compatablePlatforms");
 
-                foreach (Platform platform in CompatablePlatforms)
+                foreach (var platform in CompatablePlatforms)
+                {
                     writer.WriteElementString("platformId", platform.Id);
+                }
 
                 writer.WriteEndElement();
             }
         }
 
-        protected override void readFromXmlExtension(System.Xml.XmlNode node)
+        protected override void readFromXmlExtension(XmlNode node)
         {
             //read path
             if (node.Attributes["path"] != null)
+            {
                 Path = node.Attributes["path"].Value;
+            }
 
             //read argument pattern
             if (node.Attributes["argumentPattern"] != null)
+            {
                 ArgumentPattern = node.Attributes["argumentPattern"].Value;
+            }
 
             //read platforms
-            if (node.SelectSingleNode("compatablePlatforms") != null)
-                foreach (XmlNode platformIdNode in node.SelectSingleNode("compatablePlatforms"))
-                    compatablePlatforms.Add(ParentGameLibrary.Platforms[platformIdNode.InnerXml]);
+            var platformIds = new LinkedList<string>();
+            Common.ReadListOfStringsFromXml(node.SelectSingleNode("compatablePlatforms"), "platformId", platformIds);
+            foreach (var platformId in platformIds)
+            {
+                CompatablePlatforms.Add(ParentGameLibrary.Platforms[platformId]);
+            }
         }
         
         #endregion
@@ -79,20 +99,10 @@ namespace SomewhatGeeky.Arcadia.Engine
         {
             get
             {
-                return !string.IsNullOrEmpty(path);
+                return !string.IsNullOrEmpty(Path);
             }
         }
-        public string Path
-        {
-            get
-            {
-                return path;
-            }
-            set
-            {
-                path = value;
-            }
-        }
+
         public string FullPath
         {
             get
@@ -100,29 +110,17 @@ namespace SomewhatGeeky.Arcadia.Engine
                 return System.IO.Path.GetFullPath(Path);
             }
         }
+
         public bool FileExists
         {
             get
             {
                 if (!PathIsSet)
+                {
                     return false;
-                return System.IO.File.Exists(path);
-            }
-        }
+                }
 
-        #endregion
-
-        #region platform
-
-        public List<Platform> CompatablePlatforms
-        {
-            get
-            {
-                return compatablePlatforms;
-            }
-            set
-            {
-                compatablePlatforms = value;
+                return File.Exists(Path);
             }
         }
 
@@ -130,52 +128,20 @@ namespace SomewhatGeeky.Arcadia.Engine
 
         #region play
 
-        #region argument pattern
-        public static string ArgumentPatternFileVariable
-        {
-            get
-            {
-                return "$(FilePath)";
-            }
-        }
-        public static string DefaultArgumentPattern
-        {
-            get
-            {
-                return "\"" + ArgumentPatternFileVariable + "\"";
-            }
-        }
-        public string ArgumentPattern
-        {
-            get
-            {
-                return argumentPattern;
-            }
-            set
-            {
-                argumentPattern = value;
-            }
-        }
         public bool ArgumentPatternIsSet
         {
             get
             {
-                return !string.IsNullOrEmpty(argumentPattern);
+                return !string.IsNullOrEmpty(ArgumentPattern);
             }
         }
 
-        #endregion
-
         public void OpenGame(Game game)
         {
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            var process = new Process();
             process.StartInfo.FileName = this.Path;
 
-            string pattern;
-            if (ArgumentPatternIsSet)
-                pattern = ArgumentPattern;
-            else
-                pattern = DefaultArgumentPattern;
+            var pattern = ArgumentPatternIsSet ? ArgumentPattern : DefaultArgumentPattern;
 
             process.StartInfo.Arguments = pattern.Replace(ArgumentPatternFileVariable, game.FullPath);
 
@@ -183,9 +149,10 @@ namespace SomewhatGeeky.Arcadia.Engine
             {
                 process.Start();
             }
-            catch
+            catch(Exception exception)
             {
-                throw new Exception("Error starting emulator.");
+                //TODO: throw more specific exception
+                throw new Exception("Error starting emulator.", exception);
             }
         }
 
