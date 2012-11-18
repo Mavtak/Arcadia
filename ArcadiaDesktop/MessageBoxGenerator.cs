ebb2ac16e1219d0a33e4e0ab95d14168aa786391
David McGrath <github.com@davidmcgrath.com>
2012-11-18 14:36:30 -0500

    updated MessageBoxGenerator to use Windows Forms message boxes
     * so that the buttons are styled properly

--- ArcadiaDesktop/MessageBoxGenerator.cs
+++ ArcadiaDesktop/MessageBoxGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace SomewhatGeeky.Arcadia.Desktop
 {
@@ -8,7 +9,22 @@ namespace SomewhatGeeky.Arcadia.Desktop
     {
         public static MessageBoxResult ShowMessageBox(string message, MessageBoxButton button, MessageBoxImage image)
         {
-            return MessageBox.Show(message, GuiCommon.MainWindowBaseTitle, button, image);
+            System.Windows.Forms.Application.EnableVisualStyles();
+
+            var title = GuiCommon.MainWindowBaseTitle;
+            var formsButton = ConvertOptions(button);
+            var formsIcon = ConvertIcon(image);
+            
+            var formsResult = System.Windows.Forms.MessageBox.Show(
+                caption: title,
+                text: message,
+                buttons: formsButton,
+                icon: formsIcon
+                );
+
+            var result = ConvertResult(formsResult);
+
+            return result;
         }
 
         public static MessageBoxResult ShowMessageBox(string message)
@@ -30,6 +46,74 @@ namespace SomewhatGeeky.Arcadia.Desktop
             var icon = MessageBoxImage.Error;
 
             ShowMessageBox(message, options, icon);
+        }
+
+        private static MessageBoxButtons ConvertOptions(MessageBoxButton options)
+        {
+            switch (options)
+            {
+                case MessageBoxButton.OKCancel:
+                    return MessageBoxButtons.OKCancel;
+
+                case MessageBoxButton.YesNo:
+                    return MessageBoxButtons.YesNo;
+
+                case MessageBoxButton.YesNoCancel:
+                    return MessageBoxButtons.YesNoCancel;
+
+                case MessageBoxButton.OK:
+                default:
+                    return MessageBoxButtons.OK;
+            }
+        }
+
+        private static MessageBoxIcon ConvertIcon(MessageBoxImage icon)
+        {
+            switch(icon)
+            {
+                case MessageBoxImage.Error:
+                    return MessageBoxIcon.Error;
+
+                case MessageBoxImage.Information:
+                    return MessageBoxIcon.Information;
+
+                case MessageBoxImage.None:
+                    return MessageBoxIcon.None;
+
+                case MessageBoxImage.Question:
+                    return MessageBoxIcon.Question;
+
+                case MessageBoxImage.Warning:
+                    return MessageBoxIcon.Warning;
+
+                default:
+                    return MessageBoxIcon.None;
+            }
+        }
+
+        private static MessageBoxResult ConvertResult(DialogResult formsResult)
+        {
+            switch (formsResult)
+            {
+                case DialogResult.Abort:
+                case DialogResult.Cancel:
+                    return MessageBoxResult.Cancel;
+
+                case DialogResult.No:
+                    return MessageBoxResult.No;
+
+                case DialogResult.OK:
+                    return MessageBoxResult.OK;
+
+                case DialogResult.Yes:
+                    return MessageBoxResult.Yes;
+
+                case DialogResult.None:
+                case DialogResult.Ignore:
+                case DialogResult.Retry:
+                default:
+                    return MessageBoxResult.None;
+            }
         }
     }
 }
