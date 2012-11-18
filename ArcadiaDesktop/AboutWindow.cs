744f969958d68a0fd62a60c98c0a6848af99b9d5
David McGrath <github.com@davidmcgrath.com>
2012-11-18 13:25:39 -0500

    updated AboutWindow to use StringBuilder

--- ArcadiaDesktop/AboutWindow.cs
+++ ArcadiaDesktop/AboutWindow.cs
@@ -25,18 +25,39 @@ namespace SomewhatGeeky.Arcadia.Desktop
         {
             get
             {
-                return "Welcome to " + GuiCommon.MainWindowBaseTitle + "!"
-                    + "\n"
-                    + "\n        Arcadia is an emulator frontend that manages a wide variety of classic gaming system files.  I design it to be as configurable as the users want and yet still be easy to use right for the first time.  Going forward I hope to build an online community around Arcadia where users can share game ratings, reviews, links, and game saves.  I also hope to incorporate Arcadia into social networking websites like Facebook and Twitter.  All of this is purely for my own recreation, so I hope you guys are patient with me, and as excited as I am!"
-                    + "\n        Look around, enjoy, and email any comments to " + Arcadia.Engine.Common.ContactEmail + ".  I really appreciate your feedback."
-                    + "\n        For the latest information, check out posts about Arcadia on my blog: davidmcgrath.com/arcadia"
-                    + "\n"
-                    + "\nThanks,"
-                    + "\nDavid McGrath"
-                    + "\n\n"
-                    + "\n Arcadia.Desktop Version: " + GuiCommon.Version + " (built " + (GuiCommon.BuildTime.HasValue?GuiCommon.BuildTime.ToString():"unknown") + ")"
-                    + "\n Arcadia.Engine Version: " + Arcadia.Engine.Common.LibraryVersion + " (built " + (Arcadia.Engine.Common.BuildTime.HasValue?Arcadia.Engine.Common.BuildTime.ToString():"unknown") + ")"
-                    ;
+                var builder = new StringBuilder();
+
+                builder.Append("Welcome to ");
+                builder.Append(GuiCommon.MainWindowBaseTitle);
+                builder.Append("!");
+                builder.Append("\n");
+
+                builder.Append("\n        Arcadia is an emulator frontend that manages a wide variety of classic gaming system files.  I design it to be as configurable as the users want and yet still be easy to use right for the first time.  Going forward I hope to build an online community around Arcadia where users can share game ratings, reviews, links, and game saves.  I also hope to incorporate Arcadia into social networking websites like Facebook and Twitter.  All of this is purely for my own recreation, so I hope you guys are patient with me, and as excited as I am!");
+                builder.Append("\n        Look around, enjoy, and email any comments to " + Arcadia.Engine.Common.ContactEmail + ".  I really appreciate your feedback.");
+                builder.Append("\n        For the latest information, check out posts about Arcadia on my blog: davidmcgrath.com/arcadia");
+                builder.Append("\n");
+                builder.Append("\nThanks,");
+                builder.Append("\nDavid McGrath");
+
+                builder.Append("\n\n");
+
+                AppendVersion(builder, "Arcadia.Desktop", GuiCommon.Version, GuiCommon.BuildTime);
+                AppendVersion(builder, "Arcadia.Engine", Engine.Common.LibraryVersion, Engine.Common.BuildTime);
+
+                return builder.ToString();
+            }
+        }
+
+        private static void AppendVersion(StringBuilder builder, string component, Version version, DateTime? timestamp)
+        {
+            builder.Append("\n");
+            builder.Append(component);
+            builder.Append(": Version = ");
+            builder.Append(version);
+            if (timestamp.HasValue)
+            {
+                builder.Append(" Timestamp = ");
+                builder.Append(timestamp);
             }
         }
     }
